9f23b9ddcaff9d31544b44a522d82e7ba96398cd
斌斌斌 <1587443838@qq.com>
2020-03-29 10:39:34 +0800

    构建项目、配置全局样式、路由、element-ui

---
+++ src/views/Login/index.vue
@@ -0,0 +1,16 @@
+<template>
+    <div id="login">
+        <h1>lo</h1>
+    </div>
+</template>
+<script>
+    export default{
+        name:'login'
+    }
+</script>
+<style lang="scss" scope>
+    #login{
+        height:50vh;
+        background-color:pink;
+    }
+</style>
